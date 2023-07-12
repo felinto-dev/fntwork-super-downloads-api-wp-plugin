0fcfb3b50aa4307443242578bb286508b572ea9e
Felinto <emersong20.email@gmail.com>
2023-07-12 06:08:48 +0000

    update plugin to version 1.0.4

--- fntwork-super-downloads-api.php
+++ fntwork-super-downloads-api.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Super Downloads API
  * Plugin URI:        https://t.me/felinto
  * Description:       Plugin para integração do WordPress com a plataforma Super Downloads API.
- * Version:           1.0.3
+ * Version:           1.0.4
  * Author:            Felinto
  * Author URI:        https://t.me/felinto
  * License:           GPL-2.0+
