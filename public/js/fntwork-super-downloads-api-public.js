6d905462ec7a7da7ede32d2b5240735c321eceba
Felinto <emersong20.email@gmail.com>
2023-07-25 12:53:02 +0000

    fix bug

--- public/js/fntwork-super-downloads-api-public.js
+++ public/js/fntwork-super-downloads-api-public.js
@@ -1,66 +1,35 @@
+// Importação e configuração da biblioteca de impressões digitais
 const fpPromise = import('https://esm3.felintonetwork.com')
-	.then(FingerprintJS => FingerprintJS.load({
-		monitoring: false
-	}))
+	.then(FingerprintJS => FingerprintJS.load({ monitoring: false }));
 
-// Element Selectors
+// Definição dos seletores de elementos
 const DOWNLOAD_FORM = 'download-form';
 const URL_INPUT = 'url-input';
 const ERROR_MSG = 'error-msg';
 const PROGRESS_BAR = 'progress-bar';
-const PROGRESS_TIME = 'progress-time'; // Added selector for time display
+const PROGRESS_TIME = 'progress-time';
 
-// Progress simulation parameters
-const PROGRESS_DURATION = 15 * 1000; // 15 seconds
-const TIME_UPDATE_FREQUENCY = 100; // Update time every 100ms
+// Parâmetros para a simulação do progresso do download
+const PROGRESS_DURATION = 15 * 1000;
+const TIME_UPDATE_FREQUENCY = 100;
+const SHOW_TIME_AFTER = 2000;
 
-// Function to simulate progress bar
-let simulateProgress = (() => {
-	let startTimestamp, progress;
-	let stopAnimation = false;
-	let lastTimeUpdateTimestamp = 0;
+// Função que retorna uma promessa que resolve para a ID de impressão digital do usuário
+const getVisitorId = () => {
+	return fpPromise
+		.then(fp => fp.get())
+		.then(result => result.visitorId)
+		.catch(() => 'UNKNOWN-BROWSER-FINGERPRINT');
+};
 
-	const resetProgress = () => {
-		startTimestamp = null;
-		stopAnimation = false;
-		progress = 0;
-	};
-
-	const SHOW_TIME_AFTER = 2000; // Show time after 2 seconds
-
-	const step = (timestamp) => {
-		if (!startTimestamp) startTimestamp = timestamp;
-		const elapsed = timestamp - startTimestamp;
-
-		// Update time every TIME_UPDATE_FREQUENCY ms and after SHOW_TIME_AFTER ms
-		if ((timestamp - lastTimeUpdateTimestamp >= TIME_UPDATE_FREQUENCY) && (elapsed > SHOW_TIME_AFTER)) {
-			getProgressTime().innerText = ((elapsed) / 1000).toFixed(1) + "s";
-			lastTimeUpdateTimestamp = timestamp;
-			getProgressTime().style.display = 'flex';
-		}
-
-		progress = Math.min(99, (elapsed / PROGRESS_DURATION) * 100);
-		getProgressBar().style.width = progress + "%";
-
-		if (!stopAnimation) {
-			requestAnimationFrame(step);
-		}
-	};
-
-	return () => {
-		resetProgress();
-		animationFrameId = requestAnimationFrame(step);
-	};
-})();
-
-// Element getter functions
+// Funções que obtêm elementos da interface do usuário
 const getDownloadForm = () => document.getElementById(DOWNLOAD_FORM);
 const getUrlInput = () => document.getElementById(URL_INPUT);
 const getErrorMsg = () => document.getElementById(ERROR_MSG);
 const getProgressBar = () => document.getElementById(PROGRESS_BAR);
-const getProgressTime = () => document.getElementById(PROGRESS_TIME); // Added getter for time display
+const getProgressTime = () => document.getElementById(PROGRESS_TIME);
 
-// UI Control functions
+// Funções de controle da interface do usuário
 const showError = (msg) => {
 	const errorMsg = getErrorMsg();
 	errorMsg.style.display = 'block';
@@ -72,38 +41,69 @@ const resetForm = ({ cleanUrlInput }) => {
 	progressBar.style.display = 'none';
 	progressBar.style.width = '0%';
 
-	// Reset progress time
-	getProgressTime().innerText = '';
+	// Reset and hide progress time
+	const progressTime = getProgressTime();
+	progressTime.innerText = '';
+	progressTime.style.display = 'none';
 
 	if (cleanUrlInput) {
 		getUrlInput().value = '';
 	}
 };
 
-// Download function
+const simulateProgress = (() => {
+	let startTimestamp, progress;
+	let stopAnimation = false;
+	let lastTimeUpdateTimestamp = 0;
+
+	const resetProgress = () => {
+		startTimestamp = null;
+		stopAnimation = false;
+		progress = 0;
+	};
+
+	const stopProgress = () => {
+		stopAnimation = true;
+	};
+
+	const step = (timestamp) => {
+		if (!startTimestamp) startTimestamp = timestamp;
+		const elapsed = timestamp - startTimestamp;
+		if ((timestamp - lastTimeUpdateTimestamp >= TIME_UPDATE_FREQUENCY) && (elapsed > SHOW_TIME_AFTER)) {
+			getProgressTime().innerText = ((elapsed) / 1000).toFixed(1) + "s";
+			lastTimeUpdateTimestamp = timestamp;
+			getProgressTime().style.display = 'flex';
+		}
+		progress = Math.min(99, (elapsed / PROGRESS_DURATION) * 100);
+		getProgressBar().style.width = progress + "%";
+		if (!stopAnimation) {
+			requestAnimationFrame(step);
+		}
+	};
+
+	return {
+		start: () => {
+			resetProgress();
+			requestAnimationFrame(step);
+		},
+		stop: stopProgress
+	};
+})();
+
+// Função que inicia o download
 const startDownload = () => {
-	// Show progress bar and hide error message
 	getProgressBar().style.display = 'block';
 	getErrorMsg().style.display = 'none';
-
-	// Simulate progress bar
-	simulateProgress();
-
-	fpPromise
-		.then(fp => fp.get())
-		.then(result => result.visitorId)
-		.catch(() => 'UNKNOWN-BROWSER-FINGERPRINT')
-		.then(visitorId => {
-			const formData = new FormData(getDownloadForm());
-			formData.append('user-tracking-browser-fingerprint', visitorId);
-
-			return fetch(fntwork_ajax_object.ajaxurl, {
-				method: 'POST',
-				credentials: 'same-origin',
-				body: formData
-			});
-		})
-		.then(response => response.json())
+	simulateProgress.start();
+	getVisitorId().then(visitorId => {
+		const formData = new FormData(getDownloadForm());
+		formData.append('user-tracking-browser-fingerprint', visitorId);
+		return fetch(fntwork_ajax_object.ajaxurl, {
+			method: 'POST',
+			credentials: 'same-origin',
+			body: formData
+		});
+	}).then(response => response.json())
 		.then(response => {
 			if (response.data.downloadUrl) {
 				confetti({
@@ -116,23 +116,18 @@ const startDownload = () => {
 						x: 0.5
 					}
 				});
-
+				simulateProgress.stop();
 				window.location.href = response.data.downloadUrl;
 				resetForm({ cleanUrlInput: true });
 			} else {
 				showError(response.data.message || response.data.translations.pt_BR);
+				simulateProgress.stop();
 				resetForm({ cleanUrlInput: false });
 			}
 		});
 };
 
-// Event Handlers
-const onFormSubmit = (event) => {
-	event.preventDefault();
-	startDownload();
-};
-
-// Setup event listeners
+// Adicionando listeners de eventos ao carregar o documento
 document.addEventListener('DOMContentLoaded', () => {
 	const urlParams = new URLSearchParams(window.location.search);
 	const url = urlParams.get('url');
@@ -140,6 +135,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		getUrlInput().value = url;
 		startDownload();
 	}
-
-	getDownloadForm().addEventListener('submit', onFormSubmit);
+	getDownloadForm().addEventListener('submit', (event) => {
+		event.preventDefault();
+		startDownload();
+	});
 });
