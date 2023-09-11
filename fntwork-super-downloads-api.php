1c2f030be4976be94d3aef10957ff351c11d46d9
Felinto <emersong20.email@gmail.com>
2023-09-11 12:46:56 +0000

    create function to get providers

--- fntwork-super-downloads-api.php
+++ fntwork-super-downloads-api.php
@@ -28,11 +28,11 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if (!defined('WPINC')) {
 	die;
 }
 
-if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
 	require_once __DIR__ . '/vendor/autoload.php';
 }
 
@@ -41,14 +41,15 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'FNTWORK_SUPER_DOWNLOADS_API_VERSION', '1.0.24' );
+define('FNTWORK_SUPER_DOWNLOADS_API_VERSION', '1.0.24');
 
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-fntwork-super-downloads-api-activator.php
  */
-function activate_fntwork_super_downloads_api() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-fntwork-super-downloads-api-activator.php';
+function activate_fntwork_super_downloads_api()
+{
+	require_once plugin_dir_path(__FILE__) . 'includes/class-fntwork-super-downloads-api-activator.php';
 	Fntwork_Super_Downloads_Api_Activator::activate();
 }
 
@@ -56,19 +57,20 @@ function activate_fntwork_super_downloads_api() {
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-fntwork-super-downloads-api-deactivator.php
  */
-function deactivate_fntwork_super_downloads_api() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-fntwork-super-downloads-api-deactivator.php';
+function deactivate_fntwork_super_downloads_api()
+{
+	require_once plugin_dir_path(__FILE__) . 'includes/class-fntwork-super-downloads-api-deactivator.php';
 	Fntwork_Super_Downloads_Api_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_fntwork_super_downloads_api' );
-register_deactivation_hook( __FILE__, 'deactivate_fntwork_super_downloads_api' );
+register_activation_hook(__FILE__, 'activate_fntwork_super_downloads_api');
+register_deactivation_hook(__FILE__, 'deactivate_fntwork_super_downloads_api');
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-fntwork-super-downloads-api.php';
+require plugin_dir_path(__FILE__) . 'includes/class-fntwork-super-downloads-api.php';
 
 /**
  * Begins execution of the plugin.
@@ -79,10 +81,21 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-fntwork-super-downloads-ap
  *
  * @since    1.0.0
  */
-function run_fntwork_super_downloads_api() {
+function run_fntwork_super_downloads_api()
+{
 
 	$plugin = new Fntwork_Super_Downloads_Api();
 	$plugin->run();
-
 }
+
+function fntwork_super_downloads_api_get_providers()
+{
+	if (!class_exists('Fntwork_Super_Downloads_API_Manager')) {
+		return [];
+	}
+
+	$api_manager = new Fntwork_Super_Downloads_API_Manager('fntwork-super-downloads-api', FNTWORK_SUPER_DOWNLOADS_API_VERSION);
+	return $api_manager->get_providers();
+}
+
 run_fntwork_super_downloads_api();
