44a2e274f1dfc27950030ccc5209de71c8da2c46
Felinto <emersong20.email@gmail.com>
2023-09-12 04:50:25 +0000

    create class to act as download guard

---
+++ includes/class-fntwork-super-download-api-guard.php
@@ -0,0 +1,13 @@
+<?php
+
+class Fntwork_Super_Downloads_Api_Guard
+{
+	private $plugin_name;
+	private $version;
+
+	public function __construct($plugin_name, $version)
+	{
+		$this->plugin_name = $plugin_name;
+		$this->version = $version;
+	}
+}
