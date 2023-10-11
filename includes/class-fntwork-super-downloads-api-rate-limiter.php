fd3453afcf720e0878187cc60bd632b7a3a40060
Felinto <emersong20.email@gmail.com>
2023-10-11 10:41:36 +0000

    add function to set credits left

--- includes/class-fntwork-super-downloads-api-rate-limiter.php
+++ includes/class-fntwork-super-downloads-api-rate-limiter.php
@@ -19,20 +19,39 @@ class Fntwork_Super_Downloads_Api_Rate_Limiter
 		$this->settings_manager = $settings_manager;
 	}
 
+	private function get_transient_key(Int $user_id)
+	{
+		return "user_{$user_id}_{$this->key}";
+	}
+
 	public function get_credits_left(Int $user_id)
 	{
 		if (!$user_id) {
 			$user_id = get_current_user_id();
 		}
 
-		$transient_key = "user_{$user_id}_{$this->key}";
-		$transient_value = get_transient($transient_key);
+		$transient_value = get_transient($this->get_transient_key($user_id));
 
 		if (!$transient_value) {
 			return $this->settings_manager->get_daily_download_limit();
 		}
 
 		return $transient_value;
+	}
+
+	public function set_credits_left(Int $user_id, Int $credits_left)
+	{
+		$current_time = current_time('timestamp');
+		$midnight = strtotime('tomorrow midnight', $current_time);
+		$transient_expires_time = $midnight - $current_time;
+
+		if ($transient_expires_time >= 1 && $transient_expires_time <= 86400) {
+			set_transient(
+				$this->get_transient_key($user_id),
+				$credits_left,
+				$transient_expires_time
+			);
+		}
 	}
 
 	public function add_user_credits_left_table_list_column($column)
